4d50681499ce657d28db9724591b026ac4573dbc
Artem <artemm3note@gmail.com>
2024-01-28 01:12:23 +0200

    Added couple of test for ship factory

--- src/scripts/ShipFactory.js
+++ src/scripts/ShipFactory.js
@@ -1,31 +1,53 @@
+import { transform } from "@babel/core"
 import { Ship } from "./Ship"
 
 export class ShipFactory {
     constructor() {
-        this.shipSet = []
-        this.amount = {
-            '4': 1,
-            '3': 2,
-            '2': 3,
-            '1': 4
+        this.shipSet = {
+            huge: [],
+            big: [],
+            medium: [],
+            small: []
         }
     }
 
-    createShip(size) {
-        if (this.alreadyExsits(size)) {
-            throw new Error(`Set of ship(s) with this size (${size}) has already fulled`)
-        }
-
-        const ship = new Ship(size)
-
-        this.amount[size.toString()] =- 1
-    }
-
-    alreadyExsits(size) {
-        if (this.amount[size.toString()] > 0) {
-            return false
+    createHugeShip() {
+        if (this.shipSet.huge.length < 1) {
+            const ship = new Ship(4)
+            this.shipSet.huge.push(ship)
+            return ship
         } else {
-            return true
+            throw new Error('Set of huge ships has already fulled')
+        }
+    }
+
+    createBigShip() {
+        if (this.shipSet.big.length < 2) {
+            const ship = new Ship(3)
+            this.shipSet.big.push(ship)
+            return ship
+        } else {
+            throw new Error('Set of big ships has already fulled')
+        }
+    }
+
+    createMediumShip() {
+        if (this.shipSet.huge.length < 3) {
+            const ship = new Ship(2)
+            this.shipSet.medium.push(ship)
+            return ship
+        } else {
+            throw new Error('Set of medium ships has already fulled')
+        }
+    }
+
+    createSmallShip() {
+        if (this.shipSet.huge.length < 4) {
+            const ship = new Ship(1)
+            this.shipSet.small.push(ship)
+            return ship
+        } else {
+            throw new Error('Set of small ships has already fulled')
         }
     }
 }
---
+++ tests/ShipFactory.test.js
@@ -0,0 +1,25 @@
+import { Ship } from "../src/scripts/Ship";
+import { ShipFactory } from "../src/scripts/ShipFactory";
+
+test('Ship Factory: basic scenario of creating new ship', () => {
+    const shipFactory = new ShipFactory()
+    shipFactory.createHugeShip()
+    shipFactory.createMediumShip()
+    shipFactory.createMediumShip()
+
+    expect(shipFactory.shipSet).toEqual({
+        huge: [new Ship(4)],
+        big: [],
+        medium: [new Ship(2), new Ship(2)],
+        small: []
+    })
+})
+
+test('Ship Factory: try to create more ship that rules allows', () => {
+    const shipFactory = new ShipFactory()
+
+    expect(() => {
+        shipFactory.createHugeShip()
+        shipFactory.createHugeShip()
+    }).toThrow('Set of huge ships has already fulled')
+})
